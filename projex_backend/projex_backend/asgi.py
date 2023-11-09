4ce2ee28625a1bdb1c4e96455ca40bec03820089
Hardik Narang <126074137+Hardik-2311@users.noreply.github.com>
2023-11-09 17:09:16 +0530

    channels done

--- projex_backend/projex_backend/asgi.py
+++ projex_backend/projex_backend/asgi.py
@@ -1,16 +1,12 @@
-"""
-ASGI config for projex_backend project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
 
 import os
-
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from projexApp.channels.routing import routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projex_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": URLRouter(routing.websocket_urlpatterns),
+})
