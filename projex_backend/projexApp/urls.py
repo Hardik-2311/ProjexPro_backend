7bc1563f5b8f4d2b1b1a6916d6a7492cbe0bf481
Hardik Narang <126074137+Hardik-2311@users.noreply.github.com>
2023-11-09 18:19:15 +0530

    urls done

--- projex_backend/projexApp/urls.py
+++ projex_backend/projexApp/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from projexApp.views import *
-
 from projexApp.views.Oauth import login_direct, check_login, Oauth2_Login, logout_direct
 
 router = DefaultRouter()
 router.register(r"users", UserViewSet)
 router.register(r"projects", ProjectViewSet)
+router.register(r"tasks", ProjectViewSet)
+router.register(r"goals", ProjectViewSet)
+router.register(r"feedbacks", ProjectViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
--- projex_backend/projexApp/views/__init__.py
+++ projex_backend/projexApp/views/__init__.py
@@ -1,2 +1,5 @@
 from .Userview import *
 from .Projectview import *
+from .Goalview  import *
+from .Taskview import *
+from .Feedbackview import *
